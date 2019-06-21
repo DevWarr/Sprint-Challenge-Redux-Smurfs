7f09c86bef2d7aac4baea0bd16da4ee786c8e6f2
Devin Warrick <dev.warr@gmail.com>
2019-06-21 12:32:46 -0500

    Removed comments from App.js.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -4,12 +4,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { NavBar, SmurfForm, Smurfs } from "./";
 
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
 class App extends Component {
     render() {
         return (
