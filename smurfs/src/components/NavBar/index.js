5932c904d4ff28db04b020c6d2df9b17fb96a878
Devin Warrick <dev.warr@gmail.com>
2019-06-21 12:41:48 -0500

    Formatted SC files.

--- smurfs/src/components/NavBar/index.js
+++ smurfs/src/components/NavBar/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Nav, Title, Link } from "./NavBarSC";
 
-const NavBar = props => {
+const NavBar = () => {
     return (
         <Nav>
             <Title>Smurf Village</Title>
             <Link exact to="/" activeClassName="active">Smurfs</Link>
             <Link to="/smurf-form" activeClassName="active">New Smurf</Link>
-
         </Nav>
     );
 }
